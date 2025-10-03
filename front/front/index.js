0bc3088570410cedc713de411858c2109348d7fa
lucas-carva <lucasverissimo93@gmail.com>
2025-10-03 16:24:54 -0300

    fixing mistake on previous commit

--- front/front/index.js
+++
@@ -1,6 +0,0 @@
-import { setupPlanet } from "./src/three/planet.js"; 
-import { setupLocationSearch } from './src/ui/searchBar.js';
-
-const { earthGroup } = setupPlanet();
-
-setupLocationSearch(earthGroup);
